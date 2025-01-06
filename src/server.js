3218f0a25c0b668add559812cf5d680ef4398e77
mingming0865 <mingming0865@gmail.com>
2025-01-06 15:59:20 +0700

    Add view engines

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+
+const app = express()
+const port = 8080
+
+configViewEngine(app)
+
+app.get('/', (req, res) => {
+    res.render('test/index.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.send(`I'm Man`)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
